1c8fe41d5c55616a3690acfb89b452d60e74a4cd
yavuzomer15 <omeryavuz1542@gmail.com>
2026-01-20 02:43:56 +0300

    #3

--- transfer_learning.py
+++ transfer_learning.py
@@ -3,6 +3,13 @@ import data_preprocessing
 import torchvision.models as models
 import torch
 from data_exp import device as device
+from data_preprocessing import dataset_sizes
+from data_preprocessing import dataLoaders
+import time
+import copy
+
+device = torch.device("cpu") 
+print(f"⚠️ GPU Uyumsuzluğu Nedeniyle CPU Modu Aktif: {device}")
 
 model= models.resnet50(weights='DEFAULT')
 
@@ -26,14 +33,52 @@ print("Loss function and optimizer arranged!")
 
 #epoch
 
-def train_model(model, inputs, labels, optimizer, criterion):
-    
-    optimizer.zero_grad()
+def train_model(model, criterion, optimizer, num_epochs=1):
+    best_model_wg=copy.deepcopy(model.state_dict())
+    best_acc=0.0
 
-    outputs = model(inputs)
+    for epoch in range(num_epochs):
+        print("Epoch starts..")
 
-    loss = criterion(outputs, labels)
+        for phase in ['train', 'test']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
 
-    loss.backward()
+            running_loss=0.0
+            running_corrects=0
 
-    optimizer.step()
+            for inputs,labels in dataLoaders[phase]:
+                inputs=inputs.to(device)
+                labels=labels.to(device)
+
+                optimizer.zero_grad()
+
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs=model(inputs)
+                    _, preds = torch.max(outputs,1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+                        
+                running_loss+=loss.item()* inputs.size(0)
+                running_corrects+=torch.sum(preds==labels.data)
+
+            epoch_loss= running_loss / dataset_sizes[phase]
+            epoch_acc=running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+            if phase == 'test' and epoch_acc > best_acc:
+                best_acc=epoch_acc
+                best_model_wg=copy.deepcopy(model.state_dict())
+        print()
+    model.load_state_dict(best_model_wg)        
+    return model
+
+model_ft=train_model(model,criterion,optimizer,num_epochs=10)
+
